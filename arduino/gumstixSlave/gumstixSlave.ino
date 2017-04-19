dec622f22b55088f0f64ef3e6d4b0ab720376d52
Eric Mooney <eric.c.mooney@gmail.com>
2017-04-19 18:08:59 -0400

    add more concise slave code, before servo

--- arduino/gumstixSlave/gumstixSlave.ino
+++ arduino/gumstixSlave/gumstixSlave.ino
@@ -1,7 +1,8 @@
 /*
+  I2C code adapted from:
   https://github.com/scottellis/overo-arduino-i2c/blob/master/arduino/arduino_i2c_echo/arduino_i2c_echo.pde
-  The Arduino as an I2C slave.
-  Echo back data received converted to uppercase.
+  https://www.arduino.cc/en/Tutorial/MasterWriter
+  The Arduino as an I2C slave to power PWM channels for servos.
 */
 
 #include <Wire.h>
@@ -9,72 +10,41 @@
 #define I2C_SLAVE_ADDRESS 0x10
 #define DES_BAUD_RATE 9600
 
-// BUFFER_LENGTH comes from Wire.h
-// It is the default Arduino I2C size limit for a single transfer.
-// Larger transfers need to be broken up.
-// Not doing that here.
-uint8_t buff[BUFFER_LENGTH];
-
-int pos;
-
-// If the buffer overflows, reset to zero ;-)
 void onRecieveHandler(int numBytes)
 {
     Serial.println("received:");
-    for (int i = 0; i < numBytes; i++) {
-        buff[pos] = Wire.read();
-        Serial.println(buff[pos]);  // print each byte as an integer
-        pos++;
-
-        if (pos >= BUFFER_LENGTH)
-            pos = 0; 
+    if(Wire.available() == 4) {
+      int arm = Wire.read();       // receive byte as an integer  (arm number to move)
+      char delim = Wire.read();    // receive byte as a character (colon as a delimiter)
+      int angle = Wire.read();     // receive byte as an integer  (Arm position)
+      char term = Wire.read();     // receive byte as a character (null byte at end of string)
+      Serial.println(arm);
+      Serial.println(delim);
+      Serial.println(angle);
+      Serial.println(term);  
+    
+    } else {
+      Serial.println("Wire > 4");
+      Serial.println(Wire.available());
     }
-}
-
-void onRequestHandler()
-{
-    if (!pos)
-        return;
-
-    Serial.println("req:");
-    for (int i = 0; i < pos; i++){
-        Serial.println(buff[i]);
-        buff[i] = toupper(buff[i]);
-    }
-    Wire.write(buff, pos);
-
-    pos = 0; 
+    
 }
 
 /*
-  With power means, enable pins 2 and 3 for ground and
-  power respectively. Puts power close to the 4,5 pins
-  used for I2C. Set withPower=0 if you don't want this.
+  Setup the I2C communication
 */
-void begin_i2c(int withPower)
+void begin_i2c()
 {
-    byte pwrpin = PORTC3;
-    byte gndpin = PORTC2;
-  
-    if (withPower) {
-        DDRC |= _BV(pwrpin) | _BV(gndpin);  // make outputs
-        PORTC &=~ _BV(gndpin);
-        PORTC |=  _BV(pwrpin);
-        delay(100);
-    }
-  
-    Wire.begin(I2C_SLAVE_ADDRESS);
-    
-    Wire.onReceive(onRecieveHandler);
-    Wire.onRequest(onRequestHandler);
+    Wire.begin(I2C_SLAVE_ADDRESS);    // Connect Arduino to I2C bus at I2C_SLAVE_ADDRESS
+    Wire.onReceive(onRecieveHandler); // Create callback function for receiving data
 }
 
 void setup()
 {
     Serial.begin(DES_BAUD_RATE);
-    while (!Serial);             // Leonardo: wait for serial monitor
+    while (!Serial);                  // Wait until Serial Monitor is open
     Serial.println("\nI2C Gumstix Slave");
-    begin_i2c(0);  
+    begin_i2c();  
 }
 
 void loop()
