fe8732946402fe2b0556a1d697de2df8cee01ac4
saacg <isaacg@bu.edu>
2017-04-20 20:22:01 -0400

    cross compiled rfcomm-server

--- bluetooth/cross_compile.sh
+++ bluetooth/cross_compile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 #/ad/eng/courses/ec/ec535/gumstix/oe/cross/bin/arm-linux-gcc -o simplescan simplescan.c -lbluetooth
-#/ad/eng/courses/ec/ec535/gumstix/oe/cross/bin/arm-linux-gcc -o server server.c -lbluetooth
-/ad/eng/courses/ec/ec535/gumstix/oe/cross/bin/arm-linux-gcc -o client client.c -lbluetooth
+/ad/eng/courses/ec/ec535/gumstix/oe/cross/bin/arm-linux-gcc -o rfcomm-server rfcomm-server.c -lbluetooth
