b559990cb9d50a4b6aec7622dbfce3f6ee763510
saacg <isaacg@bu.edu>
2017-04-20 21:06:33 -0400

    set up loop in client

--- bluetooth/rfcomm-client.c
+++ bluetooth/rfcomm-client.c
@@ -8,27 +8,30 @@ int main(int argc, char **argv)
 {
     struct sockaddr_rc addr = { 0 };
     int s, status;
-    char dest[18] = "01:23:45:67:89:AB";
+    char dest[18] = "00:80:37:2E:31:20";
 
     // allocate a socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    while(1){
+	    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    // set the connection parameters (who to connect to)
-    addr.rc_family = AF_BLUETOOTH;
-    addr.rc_channel = (uint8_t) 1;
-    str2ba( dest, &addr.rc_bdaddr );
+	    // set the connection parameters (who to connect to)
+	    addr.rc_family = AF_BLUETOOTH;
+	    addr.rc_channel = (uint8_t) 1;
+	    str2ba( dest, &addr.rc_bdaddr );
 
-    // connect to server
-    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+	    // connect to server
+	    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+	    printf("%d\n", status); 
 
-    // send a message
-    if( status == 0 ) {
-        status = write(s, "hello!", 6);
+	    // send a message
+	    if( status == 0 ) {
+		status = write(s, "hello!", 6);
+	    }
+	    
+	    if( status < 0 ) perror("uh oh");
+
+	    close(s);
     }
-
-    if( status < 0 ) perror("uh oh");
-
-    close(s);
     return 0;
 }
 
